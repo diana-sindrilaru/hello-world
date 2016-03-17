24d767bd31f84c3c3b7b9ed61133d3f955902d32
diana-sindrilaru <diana.sindrilaru@evozon.com>
2016-03-17 14:24:59 +0200

    Update of test.pl
    additionally checked for 0 value

--- test.pl
+++ test.pl
@@ -2,8 +2,10 @@
 use strict;
 use warnings;
 
-my $first_value=10;
+my $first_value=0;
 if($first_value>0)
-	{print "Positive value!";}
+	{print "Positive value!\n";}
+elsif $first_value==0
+	{print "Value cannot be evaluated!\n";}
 else
-	{print "Negative value!";}
+	{print "Negative value!\n";}
